7530094df9f0b1b46ae94f42e91f0f01fcc62d0d
21petito <21petito@gothenburgswedes.org>
2020-02-20 20:59:23 +0000

    Run file configured

---
+++ Reset.py
@@ -0,0 +1,23 @@
+Clones = [100,25,85,20,75,30,100,10,2]
+
+#food
+healthy = 50
+unhealthy = 20
+poisonous = 0
+
+#water
+low_clean = 10
+
+#air
+low_clean = 20
+atomsphere_stability = 30
+
+clone_intelligence = 95
+
+if ((clone_intelligence > intelligence) or (low_intelligence > clone_intelligence)):
+  print("Stinky")
+
+
+
+clone0 = [100,2,55,"blue"]
+clone1 = [110,2,59,"hazel"]
