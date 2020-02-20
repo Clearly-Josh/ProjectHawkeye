83c6b7ea8c416cb4d566b1a17543ee57419e39f4
WingNut21 <21wenger@gothenburgswedes.org>
2020-02-20 21:28:18 +0000

    yetet

--- Reset.py
+++ Reset.py
@@ -1,16 +1,24 @@
 #Clone Order: strength, intellect, charisma, low-strength, low-intellect, low-charisma, disabilities! 
 
-Clones = [__, __, __, __, __, __, __]
+Clones = []
 
 
 #Food Order: healthy, unhealthy, poisonous!
-Food = [__, __, __]
+Food = []
 
 #Water Order: clean, low-clean
-Water = [__, __]
+Water = []
 
 #Air Order: clean, low-clean, atmospheric-stability
-Air = [__, __, __]
+Air = []
 
         #if ((clone_intelligence > intelligence) or (low_intelligence > clone_intelligence)):
 
+
+number_array = list()
+number = input("Enter the number of elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(number)):
+  n = input("number :")
+number_array. append(int(n))
+print ('ARRAY: ',number_array)
