314d38d88d0763d116b078adc395ec031cf24140
WingNut21 <21wenger@gothenburgswedes.org>
2020-02-20 21:06:42 +0000

    array order

--- Reset.py
+++ Reset.py
@@ -1,3 +1,4 @@
+#Clone Order: strength, intellect, charisma, low-strength, low-intellect, low-charisma, disabilities, 
 Clones = [100,25,85,20,75,30,100,10,2]
 
 #food
@@ -16,8 +17,3 @@ clone_intelligence = 95
 
 if ((clone_intelligence > intelligence) or (low_intelligence > clone_intelligence)):
   print("Stinky")
-
-
-
-clone0 = [100,2,55,"blue"]
-clone1 = [110,2,59,"hazel"]
