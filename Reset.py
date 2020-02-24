b8a6d7aff7e6baaa71b8a5480e7f50223465659a
21petito <21petito@gothenburgswedes.org>
2020-02-24 20:57:20 +0000

    Update

--- Reset.py
+++ Reset.py
@@ -1,24 +1,36 @@
-#Clone Order: strength, intellect, charisma, low-strength, low-intellect, low-charisma, disabilities! 
 
+#Arrays
 Clones = []
-
-
-#Food Order: healthy, unhealthy, poisonous!
 Food = []
-
-#Water Order: clean, low-clean!
 Water = []
-
-#Air Order: clean, low-clean, atmospheric-stability!
 Air = []
+#######
 
 #Choosing Number Of Elements!
+print("------------------------------------------")
+print("Enter your Clones stats in order of:")
+print("------------------------------------------")
+print("strength, intellect, charisma")
+print("low-strength, low-intellect, low-charisma")
+print("------------------------------------------")
+
 n = int(input("Enter number of elements : ")) 
+print("------------------------------------------")
 
 # @GeeksForGeeks
 #Adding Numbers!
 for i in range(0, n): 
   ele = int(input())
-  Clones.append(ele) 
+  Clones.append(ele)
 print(Clones)
+
+
+
+
+
+
+
+
+
+
 
