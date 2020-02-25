4e71896ea8613f215b40ce06b879888ecc791ff2
21petito <21petito@gothenburgswedes.org>
2020-02-25 21:27:37 +0000

    Smoother and better

--- Reset.py
+++ Reset.py
@@ -1,42 +1,76 @@
-
 #Arrays
 Clones = []
 #######
 
 #Choosing Number Of Elements!
-print("------------------------------------------")
+print("__________________________________________")
 print("Enter your Clones stats in order of:")
-print("------------------------------------------")
+print("__________________________________________")
 print("strength, intellect, charisma")
-print("------------------------------------------")
+print("__________________________________________")
 
 n = int(input("Enter number of elements : ")) 
-print("------------------------------------------")
+print("__________________________________________")
 
 # @GeeksForGeeks
 #Adding Numbers!
 for i in range(0, n): 
   ele = int(input())
   Clones.append(ele)
-print(Clones)
+print("_______________________")
 
+########Strength###################
 if (Clones[0] >= 90):
   print ("Clones Strength - Uh-Oh")
+  print("_______________________")
 elif (Clones[0] >= 25):
   print ("Clones Strength - Yes")
+  print("_______________________")
 else:
-  print("Clones Strength - Uh Oh")
+  print("Clones Strength - Uh-Oh")
+  print("_______________________")
 
+#########Intellect################
 if (Clones[1] >= 90):
-  print ("Clones Intellect - Uh Oh")
+  print ("Clones Intellect - Uh-Oh")
+  print("_______________________")
 elif (Clones[1] >= 25):
   print ("Clones Intellect - Yes")
+  print("_______________________")
 else:
   print("Clones Intellect - Uh-Oh")
+  print("_______________________")
 
+#########Charisma##################
 if (Clones[2] >= 90):
   print ("Clones Charisma - Uh-Oh")
+  print("_______________________")
 elif (Clones[2] >= 25):
   print ("Clones Charisma - Yes")
+  print("_______________________")
 else:
-  print("Clones Charisma - Uh-Oh")
+  print("Clones Charisma - Uh-Oh")  
+  print("_______________________")
+###########End#####################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
