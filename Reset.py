f19ce700ba0dd8c4d1c18985cad6cd258641fbaa
WingNut21 <21wenger@gothenburgswedes.org>
2020-02-27 21:21:14 +0000

    Clone Data!

---
+++ Reset.py
@@ -0,0 +1,40 @@
+def Clones():
+  ########Strength###################
+  if (Clones[0] >= 90):
+    print ("Clones Strength - Uh-Oh")
+    print("_______________________")
+  elif (Clones[0] >= 25):
+    print ("Clones Strength - Yes")
+    print("_______________________")
+  else:
+    print("Clones Strength - Uh-Oh")
+    print("_______________________")
+
+  #########Intellect################
+  if (Clones[1] >= 90):
+    print ("Clones Intellect - Uh-Oh")
+    print("_______________________")
+  elif (Clones[1] >= 25):
+    print ("Clones Intellect - Yes")
+    print("_______________________")
+  else:
+    print("Clones Intellect - Uh-Oh")
+    print("_______________________")
+
+  #########Charisma##################
+  if (Clones[2] >= 90):
+    print ("Clones Charisma - Uh-Oh")
+    print("_______________________")
+  elif (Clones[2] >= 25):
+    print ("Clones Charisma - Yes")
+    print("_______________________")
+  else:
+    print("Clones Charisma - Uh-Oh")  
+    print("_______________________")
+  ###########End#####################
+
+def Continuity
+
+def Climate
+
+def Education 
