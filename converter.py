0c37cdf9eb765e772ac64ecc00d0e47cade3250c
Clearly-Josh <joshua.clark@goswedes.org>
2020-02-27 21:25:50 +0000

    Uh, uh, uh.

--- converter.py
+++ converter.py
@@ -3,14 +3,13 @@ k = open("Knowledge.txt","w+")
 def converter(what_toadd, knowledge_doc):
   new4arg = ''.join(format(ord(i), 'b') for i in what_toadd)
   knowledge_doc.write(new4arg)
+  k.close()
 
 what_toadd = "The meaning is 42."
 
 converter(what_toadd, k)
 getelfile = open("Knowledge.txt","r+")
 l = getelfile.read()
-
-open("Knowledge.txt").seek(0,0)
 
 print(l)
 #A lot of code coming up taken from https://www.geeksforgeeks.org/convert-binary-to-string-using-python/
