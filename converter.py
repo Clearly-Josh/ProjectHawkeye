092c29ae5e790abfde8f8b3672a29214fa4ae5c0
22ward <22ward@gothenburgswedes.org>
2020-02-27 21:20:11 +0000

    Commit

--- converter.py
+++ converter.py
@@ -3,20 +3,28 @@ k = open("Knowledge.txt","w+")
 def converter(what_toadd, knowledge_doc):
   new4arg = ''.join(format(ord(i), 'b') for i in what_toadd)
   knowledge_doc.write(new4arg)
-  knowledge_doc.write('''
-''')
 
 what_toadd = "The meaning is 42."
 
 converter(what_toadd, k)
-
-getelfile = open("Knowledge.txt","r")
+getelfile = open("Knowledge.txt","r+")
 l = getelfile.read()
-print(l)
 
+open("Knowledge.txt").seek(0,0)
+
+print(l)
 #A lot of code coming up taken from https://www.geeksforgeeks.org/convert-binary-to-string-using-python/
 #Purpose is getting from binary back to string
 
 def BinaryToDecimal(binary): 
   string = int(binary, 2)
   return string
+
+str_data =' '
+
+for i in range(0, len(l)):
+  Possibly_data = l[i:i + 7]
+  Notpercentage = BinaryToDecimal(Possibly_data)
+  str_data = str_data + chr(Notpercentage)
+
+print ("Secret Message:",str_data)
