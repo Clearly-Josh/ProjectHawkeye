958f846ff785887526cb53b44f39f7e0d3b9e261
MatthewShea35 <20shea@gothenburgswedes.org>
2020-02-28 21:24:30 +0000

    Added order

--- Seven.py
+++ Seven.py
@@ -1,5 +1,48 @@
-import test
-test
+import Services
+Services.services()
+import government
+government.government()
+
+
+
+
+
+
+#1 - Reset, 2 - Defense,3 - Climate, 4 - Cloning, 5- Data,6- Education, 7 - Continuity
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #execfile('test.py')
 
 
