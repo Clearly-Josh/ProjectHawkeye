bb53a7742d3d28c9d97df0d0e833850ee62bd94c
Clearly-Josh <joshua.clark@goswedes.org>
2020-03-02 14:44:01 +0000

    Driver structure in comments

--- Seven.py
+++ Seven.py
@@ -1,52 +1,41 @@
 import Services
-Services.services()
 import government
-government.government()
+
+#1 - Hazel
+#unknown call
+#Reset.Defense/Hazel(defense/hazelReturn)
 
 
 
+#2 - Climate
+#September.function(IntegerOfAirQuality)
+#September.function2(IntegerOfAWaterQuality)
+#Reset.Climate(climateReturn)
 
 
 
-#1 - Reset, 2 - Defense,3 - Climate, 4 - Cloning, 5- Data,6- Education, 7 - Continuity
+#3 - Cloning
+#unkown call
+#Reset.Cloning(cloningReturn)
 
 
 
+#4 - Data
+#converter.converter(what_toadd, knowledge_doc)
+#^I think :/
+#Reset.Data(dataReturn)
 
 
 
+#5 - Education
+#unknown call
+#Reset.education(educationReturn)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#execfile('test.py')
-
-
-
-
+#6 - Continuity
+#Services.services(unknownArg)
+#government.government(unknownArg)
+#Reset.continuity(continuityReturn)
 
 
