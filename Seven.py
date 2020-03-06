bd6cc65bdd886d1411d8d63570eae72860f68b42
Sean <22graff@gothenburgswedes.org>
2020-03-06 20:57:41 +0000

    testing converter

--- Seven.py
+++ Seven.py
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
-import Defeated
+
 #print("Seven is running..")
 
 headBadGuy = "방탄소년단"
 
-import pants
 #import Services
 #import government
-=======
+
 import Reset
 import Defeated
 import Clones
 import pants
 import services
 import government
+import converter
 
 headBadGuy = "방탄소년단"
 IntegerOfAirQuality = 1
@@ -27,26 +26,26 @@ print("Seven is running..")
 print()
 
 #Reset.reset1(str, int, chr)
->>>>>>> origin/master
+
 
 #1 - Hazel
 #Reset.Defense/Hazel(defense/hazelReturn)
-print(Defeated.bobStone(headBadGuy))
+#print(Defeated.bobStone(headBadGuy))
 
 
 #2 - Climate
 #September.function(IntegerOfAirQuality)
-<<<<<<< HEAD
+
 #September.function2(IntegerOfAWaterQuality)
-=======
+
 #September.function2(IntegerOfWaterQuality)
->>>>>>> origin/master
+
 #Reset.Climate(climateReturn)
 
 
 
 #3 - Cloning
-<<<<<<< HEAD
+
 #unkown call
 #Reset.Cloning(cloningReturn)
 
@@ -54,14 +53,14 @@ print(Defeated.bobStone(headBadGuy))
 
 #4 - Data
 #converter.converter(what_toadd, knowledge_doc)
-=======
+
 #Reset.Cloning(cloningReturn)
-clones = Clones.Cloning(100,2)
+#clones = Clones.Cloning(100,2)
 
 
 #4 - Data
 converter.converter(what_toadd, knowledge_doc)
->>>>>>> origin/master
+
 #^I think :/
 #Reset.Data(dataReturn)
 
@@ -70,21 +69,21 @@ converter.converter(what_toadd, knowledge_doc)
 #5 - Education
 #pants.education(cloneNum)
 #Reset.education(educationReturn)
-<<<<<<< HEAD
+
 cloneName = "Stacy"
-=======
->>>>>>> origin/master
-pants.education(cloneName)
+
+
+#pants.education(cloneName)
 
 
 #6 - Continuity
-<<<<<<< HEAD
+
 #Services.services(unknownArg)
 #government.government(unknownArg)
 #Reset.continuity(continuityReturn)
-=======
-services.services()
-government.government()
+
+#services.services()
+#government.government()
 #Reset.continuity(continuityReturn)
 
->>>>>>> origin/master
+
